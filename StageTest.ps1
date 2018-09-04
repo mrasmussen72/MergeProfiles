a172f58814d7341023d6a4659ae6127d03cbdf19
Michael Rasmussen <michaelrasmussen@live.com>
2018-09-04 07:49:07 -0700

    Added test files

---
+++ StageTest.ps1
@@ -0,0 +1,13 @@
+﻿$source = 'C:\dBackup - Copy'
+$dest = 'c:\dBackup'
+$newName = 'dBackup'
+$newSource = 'C:\dBackup'
+$newDest = 'c:\dBackup - Copy'
+
+Remove-Item -Path $dest -Recurse -Force
+Rename-Item -Path $source -NewName $newName -Force
+Copy-Item -Path $newSource -Destination $newDest -Recurse 
+
+Remove-Item -Path C:\Users\DAIsProfile -Recurse -Force
+Remove-Item -Path 'C:\Users\DAIsProfile - Copy' -Recurse -Force
+Remove-Item -Path 'C:\Users\DAIsProfile - Copy - Copy' -Recurse -Force

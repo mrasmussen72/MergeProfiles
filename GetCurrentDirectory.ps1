97a7fd949c1c62f39db5a3dbfdb346f01b31f43a
Michael Rasmussen <michaelrasmussen@live.com>
2018-09-10 11:15:36 -0700

    GetDirectory file added

---
+++ GetCurrentDirectory.ps1
@@ -0,0 +1,5 @@
+﻿# Determine script location for PowerShell
+$ScriptDir = Split-Path $script:MyInvocation.MyCommand.Definition 
+$temp = $script:MyInvocation.MyCommand.Definition
+ 
+Write-Host "Current script directory is $ScriptDir"

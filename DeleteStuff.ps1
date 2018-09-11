020a6e5ff6b1a50d460cfd49565ed4985d5aa00c
Michael Rasmussen <michaelrasmussen@live.com>
2018-09-11 11:31:47 -0700

    Added deleteStuff test file and staging file

---
+++ DeleteStuff.ps1
@@ -0,0 +1,25 @@
+﻿function Delete-Empties($path)
+{
+    $items = Get-ChildItem -Path $path -Recurse -Filter "*.*"
+    [long]$numOfObjects = $items.Length
+    
+    foreach($item in $items)
+    {
+        $sourcePathObject = $item.FullName
+        if((Get-Item $sourcePathObject) -is [System.IO.DirectoryInfo])
+        {
+            [long]$objsInDir = (Get-ChildItem -Path $sourcePathObject).Length
+            if($objsInDir -eq 0)
+            {
+                Remove-Item -Path $sourcePathObject
+            }
+            else
+            {
+                Delete-Empties -path $sourcePathObject
+            }
+
+        }
+    }
+}
+
+Delete-Empties -path "C:\temp\IsProfile"
---
+++ DeleteStuffStaging.psa.ps1
@@ -0,0 +1,10 @@
+﻿New-Item -Path "C:\temp\IsProfile" -ItemType Directory
+New-Item -Path "C:\temp\IsProfile\Docs" -ItemType Directory
+New-Item -Path "C:\temp\IsProfile\Vids" -ItemType Directory
+New-Item -Path "C:\temp\IsProfile\Favs" -ItemType Directory
+New-Item -Path "C:\temp\IsProfile\Docs\SomeDocs" -ItemType Directory
+New-Item -Path "C:\temp\IsProfile\Vids\SomeVids" -ItemType Directory
+New-Item -Path "C:\temp\IsProfile\Vids\SomeVids\Vid.txt" -ItemType File
+
+New-Item -Path "C:\temp\IsProfile\Docs\EmptyDocs" -ItemType Directory
+
